8de905c31b295c7a8b1ecac092692307f7931dbe
HIMANSHU ANAND <himanshuanand563@gmail.com>
2025-10-12 21:43:48 +0000

    used pink colour dark  (_for element div_)

--- src/components/ui/about-card.tsx
+++ src/components/ui/about-card.tsx
@@ -27,16 +27,14 @@ export const AboutCard = ({ name, aboutMe, imageUrl, altText }: AboutCardProps) 
           </div>
           <div className="bottom-bottom">
             <div className="social-links-container">
-              <Link href="https://www.instagram.com/h_anand_21/" target="_blank" rel="noopener noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" width={16} height="15.999" viewBox="0 0 16 15.999">
-                  <path id="Subtraction_4" data-name="Subtraction 4" d="M6-582H-2a4,4,0,0,1-4-4v-8a4,4,0,0,1,4-4H6a4,4,0,0,1,4,4v8A4,4,0,0,1,6-582ZM2-594a4,4,0,0,0-4,4,4,4,0,0,0,4,4,4,4,0,0,0,4-4A4.005,4.005,0,0,0,2-594Zm4.5-2a1,1,0,0,0-1,1,1,1,0,0,0,1,1,1,1,0,0,0,1-1A1,1,0,0,0,6.5-596ZM2-587.5A2.5,2.5,0,0,1-.5-590,2.5,2.5,0,0,1,2-592.5,2.5,2.5,0,0,1,4.5-590,2.5,2.5,0,0,1,2-587.5Z" transform="translate(6 598)" />
-                </svg>
-              </Link>
-              <Link href="https://x.com/hanand_21" target="_blank" rel="noopener noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" /></svg>
-               </Link>
-               <Link href="https://github.com/h-anand21" target="_blank" rel="noopener noreferrer">
+              <Link href="https://github.com/h-anand21" target="_blank" rel="noopener noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" /></svg>
+               </Link>
+               <Link href="https://x.com/hanand_21" target="_blank" rel="noopener noreferrer">
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" /></svg>
+               </Link>
+                <Link href="https://www.linkedin.com/in/himanshu-anand21/" target="_blank" rel="noopener noreferrer">
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 53.79-54.3c29.7 0 53.79 24.2 53.79 54.3a53.79 53.79 0 0 1-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg>
                </Link>
             </div>
           </div>
@@ -65,11 +63,11 @@ const StyledWrapper = styled.div`
     border: none;
   }
   .card .mail svg {
-    stroke: #fbb9b6;
+    stroke: #E91E63;
     stroke-width: 3px;
   }
   .card .mail svg:hover {
-    stroke: #f55d56;
+    stroke: #E91E63;
   }
   .card .profile-pic {
     position: absolute;
@@ -79,7 +77,7 @@ const StyledWrapper = styled.div`
     left: 3px;
     border-radius: 29px;
     z-index: 1;
-    border: 0px solid #fbb9b6;
+    border: 0px solid #E91E63;
     overflow: hidden;
     transition: all 0.5s ease-in-out 0.2s, z-index 0.5s ease-in-out 0.2s;
   }
@@ -98,7 +96,7 @@ const StyledWrapper = styled.div`
     bottom: 3px;
     left: 3px;
     right: 3px;
-    background: #fbb9b6;
+    background: #E91E63;
     top: 80%;
     border-radius: 29px;
     z-index: 2;
@@ -149,12 +147,12 @@ const StyledWrapper = styled.div`
     filter: drop-shadow(0 5px 5px rgba(165, 132, 130, 0.1333333333));
   }
   .card .bottom .bottom-bottom .social-links-container svg:hover {
-    fill: #f55d56;
+    fill: #E91E63;
     transform: scale(1.2);
   }
   .card .bottom .bottom-bottom .button {
     background: white;
-    color: #fbb9b6;
+    color: #E91E63;
     border: none;
     border-radius: 20px;
     font-size: 0.6rem;
@@ -162,7 +160,7 @@ const StyledWrapper = styled.div`
     box-shadow: rgba(165, 132, 130, 0.1333333333) 0px 5px 5px 0px;
   }
   .card .bottom .bottom-bottom .button:hover {
-    background: #f55d56;
+    background: #E91E63;
     color: white;
   }
   .card:hover {
@@ -172,7 +170,7 @@ const StyledWrapper = styled.div`
     top: 20%;
     border-radius: 80px 29px 29px 29px;
     transition: all 0.5s cubic-bezier(0.645, 0.045, 0.355, 1) 0.2s;
-    background: #f55d56;
+    background: #E91E63;
   }
   .card:hover .profile-pic {
     width: 100px;
@@ -182,7 +180,7 @@ const StyledWrapper = styled.div`
     left: 10px;
     border-radius: 50%;
     z-index: 3;
-    border: 7px solid #f55d56;
+    border: 7px solid #E91E63;
     box-shadow: rgba(96, 75, 74, 0.1882352941) 0px 5px 5px 0px;
     transition: all 0.5s ease-in-out, z-index 0.5s ease-in-out 0.1s;
   }
