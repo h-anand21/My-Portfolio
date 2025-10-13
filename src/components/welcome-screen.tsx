c1bd07bb5975a02e3c0cb0836634b25b51432ea3
HIMANSHU ANAND <himanshuanand563@gmail.com>
2025-10-13 00:32:26 +0000

    <button className="seal bg-primary text-primary-foreground w-10 aspect-s

--- src/components/welcome-screen.tsx
+++ src/components/welcome-screen.tsx
@@ -15,7 +15,7 @@ const WelcomeScreen = () => {
           </div>
         </div>
         <button className="seal bg-primary text-primary-foreground w-10 aspect-square rounded-full z-40 text-[10px] flex items-center justify-center font-semibold [clip-path:polygon(50%_0%,_80%_10%,_100%_35%,_100%_70%,_80%_90%,_50%_100%,_20%_90%,_0%_70%,_0%_35%,_20%_10%)] group-hover:opacity-0 transition-all duration-1000 group-hover:scale-0 group-hover:rotate-180 border-4 border-primary-foreground/50">
-          PRO
+          click here
         </button>
         <div className="tp transition-all duration-1000 group-hover:duration-100 bg-neutral-800 absolute group-hover:[clip-path:polygon(50%_0%,_100%_0,_0_0)] w-full h-full [clip-path:polygon(50%_50%,_100%_0,_0_0)]" />
         <div className="lft transition-all duration-700 absolute w-full h-full bg-neutral-900 [clip-path:polygon(50%_50%,_0_0,_0_100%)]" />
